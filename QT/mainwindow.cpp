514448171276467a39d26785771a60d1f36a68b2
terry <43752642+TrXuize@users.noreply.github.com>
2021-03-14 16:21:20 +0800

    commit

---
+++ QT/mainwindow.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b976f78edd8f449e57e261df3a64033412cfa2153efcaf4af11d6e404f0b6a25
+size 8819
---
+++ STEP2/mainwindow.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee7328be01a1123ed1244ab55b8e0ba60a612679c196cb39b1ba3c661b7ecdab
+size 542
