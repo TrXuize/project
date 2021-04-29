38a6e962a8e973c742d34ac289e5e36ab3917b9a
adasdf <43752642+TrXuize@users.noreply.github.com>
2021-04-29 10:39:02 +0800

    Delete QT directory

--- QT/main.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1d14919d5e9cf1e7fbf25816fad587b4234bee4ede7c9fdafe73b7be69b46f09
-size 445
--- QT/mainwindow.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:30f7503d439eec5b9e89cd97d9e526dacb433663508a8d506f4f8301e4464691
-size 503
